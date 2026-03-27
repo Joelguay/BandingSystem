f8943eddb5c08334ea52da6017f5a1076ad9bb5b
Joel Guay <guayjoel1@gmail.com>
2026-03-27 15:54:55 +0800

    addeed orderding and list_per_page

--- registry/admin.py
+++ registry/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+
 from .models import AuditLog, Breeder, Chicken, OwnershipHistory
+
+# Breeder Admin
 
 @admin.register(Breeder)
 class BreederAdmin(admin.ModelAdmin):
+
     list_display = ("name", "location", "contact", "is_active", "created_at")
     search_fields = ("name", "location", "contact")
+    list_filter = ("is_active",)
+    readonly_fields = ("created_at", "updated_at", "deleted_at")
+    ordering = ("-created_at",)
+    list_per_page = 25
 
+# Chicken Admin
 
 @admin.register(Chicken)
 class ChickenAdmin(admin.ModelAdmin):
+
     list_display = (
         "wingband_number",
         "breeder",
@@ -22,9 +32,23 @@ class ChickenAdmin(admin.ModelAdmin):
 
     search_fields = ("wingband_number", "breeder__name")
 
+    list_filter = ("birth_category", "is_active", "color", "breeder")
+
+    readonly_fields = (
+        "birth_category",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    )
+
+    ordering = ("-created_at",)
+    list_per_page = 25
+
+# Ownership History Admin
 
 @admin.register(OwnershipHistory)
 class OwnershipHistoryAdmin(admin.ModelAdmin):
+
     list_display = (
         "chicken",
         "owner_name",
@@ -34,9 +58,43 @@ class OwnershipHistoryAdmin(admin.ModelAdmin):
     )
 
     search_fields = ("chicken__wingband_number", "owner_name")
+    list_filter = ("date_transferred", "owner_breeder")
 
+    readonly_fields = (
+        "chicken",
+        "owner_breeder",
+        "owner_name",
+        "date_transferred",
+        "recorded_at",
+        "notes",
+    )
+
+    ordering = ("-date_transferred", "-recorded_at")
+    list_per_page = 25
+
+# Audit Log Admin
 
 @admin.register(AuditLog)
 class AuditLogAdmin(admin.ModelAdmin):
+
     list_display = ("timestamp", "user", "action", "model_name", "record_id")
     search_fields = ("model_name", "record_id", "user__username")
+    list_filter = ("action", "model_name")
+
+    readonly_fields = (
+        "user",
+        "action",
+        "model_name",
+        "record_id",
+        "details",
+        "timestamp",
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    ordering = ("-timestamp",)
+    list_per_page = 50  
