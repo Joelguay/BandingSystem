33ca008393c075644483c5935fce33769b7ac7f9
Joel Guay <guayjoel1@gmail.com>
2026-03-27 13:41:45 +0800

    Register registry models in admin

--- registry/admin.py
+++ registry/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Breeder, Chicken, OwnershipHistory, AuditLog
+
 
 # Register your models here.
+admin.site.register(Breeder)
+admin.site.register(Chicken)
+admin.site.register(OwnershipHistory)
+admin.site.register(AuditLog)
+
+
