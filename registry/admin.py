0ed2cb2cc0cb2354474a4dc70a50bae19c2fd733
Joel Guay <guayjoel1@gmail.com>
2026-03-27 14:50:18 +0800

    replace wuth ModelAdmin classes

--- registry/admin.py
+++ registry/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
-from .models import Breeder, Chicken, OwnershipHistory, AuditLog
+from .models import AuditLog, Breeder, Chicken, OwnershipHistory
+
+@admin.register(Breeder)
+class BreederAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "contact", "is_active", "created_at")
+    search_fields = ("name", "location", "contact")
 
 
-# Register your models here.
-admin.site.register(Breeder)
-admin.site.register(Chicken)
-admin.site.register(OwnershipHistory)
-admin.site.register(AuditLog)
+@admin.register(Chicken)
+class ChickenAdmin(admin.ModelAdmin):
+    list_display = (
+        "wingband_number",
+        "breeder",
+        "color",
+        "comb_type",
+        "leg_color",
+        "birth_category",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = ("wingband_number", "breeder__name")
 
 
+@admin.register(OwnershipHistory)
+class OwnershipHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "chicken",
+        "owner_name",
+        "owner_breeder",
+        "date_transferred",
+        "recorded_at",
+    )
+
+    search_fields = ("chicken__wingband_number", "owner_name")
+
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "user", "action", "model_name", "record_id")
+    search_fields = ("model_name", "record_id", "user__username")
